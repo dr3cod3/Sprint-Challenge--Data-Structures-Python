e2299a3a990180ee02c0a14bcc3283712477290e
Adebola Adesina <dr3cod3form3@outlook.com>
2020-07-17 10:31:47 -0700

    Buffer reader

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # Set the storage capacity to be empty
+        self.storage = []
+        # set the oldest to be None
+        self.oldest = None
+        self.capacity = capacity
 
     def append(self, item):
-        pass
+        # Check if the storage is empty and capacity is not reached
+        # if it is empty add the item
+        if self.capacity > 0:
+            if self.oldest == None:
+                self.storage.append(item)
+                self.oldest = 0
+        # if storage is not empty and the capacity is full
+        # Override it
+            elif len(self.storage) == self.capacity:
+                self.storage[self.oldest] = item
+
+                if self.oldest == self.capacity - 1:
+                    self.oldest = 0
+                else:
+                    self.oldest = self.oldest + 1
+
+            else:
+                self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
